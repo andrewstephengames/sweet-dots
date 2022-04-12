7cceb51bd118b8693ba4febf0c71aca23ad8977b
andrewstephengames <andrewstephengames999@gmail.com>
2022-04-13 00:46:39 +0300

    Tweaks.

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,28 +3,28 @@ xset -dpms &
 xset s off &
 xset -b &
 picom &
-xfce4-panel &
 sh $HOME/scripts/dotsync
-sh $HOME/scripts/pkgmaintain.sh
+sh $HOME/scripts/pkgmaintain
 dunst &
 redshift &
+pulseaudio &
 unclutter --timeout 3 &
-$HOME/scripts/randbg
+#sh $HOME/scripts/randbg
+feh --bg-fill /home/andrew/wallpapers/zo7q0cdgb5h81.jpg
 flameshot &
-#pcmanfm -d &
-/usr/lib/kdeconnectd -platform offscreen &
-kdeconnect-indicator &
+pcmanfm -d &
+mconnect -d &
 #dolphin &
 waterfox-g4 &
 pcmanfm &
-wmctrl -s 2
 alacritty &
 lxterminal -e $HOME/scripts/mus &
-lutris &
+fsearch &
+#lutris &
 pavucontrol &
-gpicview $HOME/Documents/timetable-darkmode.png &
-transmission-gtk &
-(sleep 10 && wmctrl -s 0 && wmctrl -s 1 && wmctrl -s 2 && wmctrl -s 3 && wmctrl -s 4 && wmctrl -s 5 &&
-wmctrl -s 6 && wmctrl -s 7 && wmctrl -s 8 && wmctrl -s 0) &
+nsxiv -s f -f -b $HOME/Documents/timetable-darkmode.png &
+ultimmc &
+#(sleep 10 && wmctrl -s 0 && wmctrl -s 1 && wmctrl -s 2 && wmctrl -s 3 && wmctrl -s 4 && wmctrl -s 5 &&
+#wmctrl -s 6 && wmctrl -s 7 && wmctrl -s 8 && wmctrl -s 0) &
 #cool-retro-term -e $HOME/scripts/mus &
 #newsflash &
